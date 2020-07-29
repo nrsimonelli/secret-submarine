2df382e7affa7919880f0f983c8a6f760fd54b07
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-07-29 15:31:51 -0500

    stretch mode

---
+++ build/precache-manifest.115a1fe8b60dbde9cd320cebc2bf9a5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f24b0cba6170f7667607add58621cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84b82edf610b0c6ac967",
+    "url": "/static/css/main.c079c376.chunk.css"
+  },
+  {
+    "revision": "6c1cbdccc6a517fe0f05",
+    "url": "/static/js/2.79b471ab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.79b471ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84b82edf610b0c6ac967",
+    "url": "/static/js/main.d0802c62.chunk.js"
+  },
+  {
+    "revision": "cfabed5267c41a13b59e",
+    "url": "/static/js/runtime-main.ddf8fe10.js"
+  },
+  {
+    "revision": "7ce9680e738565539c06cf0326d7302f",
+    "url": "/static/media/submarine.7ce9680e.svg"
+  }
+]);
